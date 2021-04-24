3d444b76362c25ef098b1f061d74c356099bdf76
gonzaloIriart <gonzaloiriart@gmail.com>
2021-04-24 17:31:06 -0300

    initial layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BindingComponent } from './componentes/binding/binding.component';
+import { EstructuraComponent } from './componentes/estructura/estructura.component';
+import { AtributosComponent } from './componentes/atributos/atributos.component';
+import { FormulariosComponent } from './componentes/formularios/formularios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BindingComponent,
+    EstructuraComponent,
+    AtributosComponent,
+    FormulariosComponent
   ],
   imports: [
     BrowserModule
